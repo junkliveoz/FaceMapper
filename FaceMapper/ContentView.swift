e5e197836ea5ee065bf945cb3eae34bc946b2644
Adam Sayer <asayer82@icloud.com>
2024-08-23 11:30:25 +1000

    Initial Commit

---
+++ FaceMapper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FaceMapper
+//
+//  Created by Adam on 23/8/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FaceMapper/FaceMapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  FaceMapperApp.swift
+//  FaceMapper
+//
+//  Created by Adam on 23/8/2024.
+//
+
+import SwiftUI
+
+@main
+struct FaceMapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
